fb326dd90e340d4fd07a3c46c11b2b24302c8f34
mojsun <m.khorashahi7@gmail.com>
2023-02-23 22:17:57 -0500

    adding info on indexjs in module file

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,17 +1,32 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "Category_id",
+});
 
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: "Category_id",
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+  },
+  as: "Product_Tag",
+});
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+  },
+  as: "Product_Tagg",
+});
 module.exports = {
   Product,
   Category,
